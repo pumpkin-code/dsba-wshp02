932a7595bd7f9ad3b660675b59ee3903ee61e7bc
Sergey Sh <bjolyk@gmail.com>
2019-11-02 16:11:03 +0300

    Outcome of workshops 2 and 3 (SSh)

--- wshp2/src/ex1/main.cpp
+++ wshp2/src/ex1/main.cpp
@@ -17,9 +17,37 @@
 // the entrypoint of the application
 int main()
 {
-    std::cout << "Hello world!";
+    std::cout << "Hello world!";        // :: is __scope__ operator
+
+    // The rule: use size_t datatype for almost all cases when you deal with sizes in C++.
+    size_t s1 = sizeof(char); // ends with ; (semicolon)
+    // ^^^ this is a definition statement
+    //       ^^^ we initialize the newly created variable with a result of
+    //           an expression, which gives a size of type char, in bytes
+
+
+    size_t s2 = sizeof(int);
+
+    using std::cout;        // this statement is used for disclosing the object
+                            // named cout from the namespace named std to be
+                            // able to use the object w/o scope operator and std prefix
+
+    //cout << "\nThe size of int datatype is equal to " << s2 << " bytes";
+    cout << std::endl << std::endl
+         <<  "The size of int datatype is equal to " << s2 << " bytes";
+
+    // the name of this :  character is colon
+
+    using std::endl;    // normally such statements are appeared at the very
+                        // begining of a method
+    cout << endl << "The size of double datatype is "
+         << sizeof(double)  << " bytes";    // this is an expression statement
+
+    //cout
 
     // TODO: place your code starting from this line
+
+    int a = 0;
 
     return 0;
 }
--- wshp2/src/ex2/main.cpp
+++ wshp2/src/ex2/main.cpp
@@ -26,9 +26,157 @@
 // the entrypoint of the application
 int main()
 {
-    std::cout << "Hello world!";
+    // a toy example: we have an object called x defined in the scope of two
+    // nested namespaces
+    //
+    //      namespace Foo {  ...  namespace Bar {  ... x  .. }    };
+    //
+    // how to refer to the object x?
+    //      Foo::Bar::x
 
-    // TODO: place your code starting from this line
+    using std::cin;         // further we want to use cin w/o std prefix
+
+    std::cout << "Multiplication table application\n\n";
+
+    // we're going to provide a prompt
+    std::cout << "Input a size of the table: ";
+
+    // here we have a defining statement
+    int n;              // we automatically allocate 4 bytes right at this very point
+                        // (where? — in the stack memory)
+
+    // for instance, if a user inputs 42 at this point, so internal representation
+    // of variable n will be like this
+    //      +----+----+----+----+
+    //      | 2A | 0  |  0 |  0 |
+    //      +----+----+----+----+
+    //
+    //       0    1    2    3     ← number of a cell/byte (Little Endian)
+
+    cin >> n;
+
+    //-------------------<Variant 3 (final)>-------------------
+    // see for vars 1 and 2 below
+
+    // check whether the input number is positive one
+    if(n <= 0)          //(!(n > 0))
+    {
+        std::cout << "The input value of n is wrong";
+        return 0;
+    }
+
+    // the only way how to reach this point is to have (n <= 0) expression
+    // evaluated as false
+
+    // we have to output a header of our table
+
+    // try to use while-loop  statement first
+    // first, we have to create a countet variable
+    int k = 1;
+
+    // output all elements of the header using the while loop
+    while(k <= n)
+        std::cout << '\t' << k++;       // correct
+
+    std::cout << std::endl;     // the same as using '\n'
+
+    // output the body of the table
+    //
+    //for(int i = 0; i < n  ;  i++)
+    for(int i = 1; i <= n; ++i)
+    {
+        // output the number of the current row
+        std::cout << i;
+        for(int j = 1; j <= n; j += 1)
+        {
+            std::cout << '\t' << i * j;
+        }
+        std::cout << '\n';     // the same as using std::endl
+    }
+    // the end of meaningful part of the program
+
+
+    //-------------------<Variant 2>-------------------
+    // below we are going to inverse the logic and swap the if and else branches
+
+    //    // check whether the input number is positive one
+    //    if(n <= 0)          //(!(n > 0))
+    //        std::cout << "The input value of n is wrong";
+    //    else
+    //    {
+    //        // we have to output a header of our table
+
+    //        // try to use while-loop  statement first
+    //        // first, we have to create a countet variable
+    //        int k = 1;
+
+    //        // output all elements of the header using the while loop
+    //        while(k <= n)
+    //            std::cout << '\t' << k++;       // correct
+
+    //        std::cout << std::endl;     // the same as using '\n'
+
+    //        // output the body of the table
+    //        //
+    //        //for(int i = 0; i < n  ;  i++)
+    //        for(int i = 1; i <= n; ++i)
+    //        {
+    //            // output the number of the current row
+    //            std::cout << i;
+    //            for(int j = 1; j <= n; j += 1)
+    //            {
+    //                std::cout << '\t' << i * j;
+    //            }
+    //            std::cout << '\n';     // the same as using std::endl
+    //        }
+    //    } // the end of meaningful part of the program
+
+
+    //-------------------<Variant 1>-------------------
+    // this variant is ok except of readability of lonf if...else statemtn
+    //    // check whether the input number is positive one
+    //    if(n > 0)
+    //    {
+    //        // we have to output a header of our table
+
+    //        // try to use while-loop  statement first
+    //        // first, we have to create a countet variable
+    //        int k = 1;
+
+    //        // output all elements of the header using the while loop
+    //        while(k <= n)
+    //        {
+    ////            std::cout << '\t' << k;
+    ////            //k = k + 1;  // a trivial approach
+    ////            //k += 1;       // this is a shortcut for ^^^ approach
+    ////            //k++;          // an increatemntal approch with postfix ++
+    ////            ++k;            // an increatemntal approch with prefix ++
+
+    //            std::cout << '\t' << k++;       // correct
+    //            //std::cout << '\t' << ++k;     // incorrect
+
+    //            // let us consider using the for-loop statement
+    //        }
+
+    //        std::cout << std::endl;     // the same as using '\n'
+
+
+    //        // output the body of the table
+    //        //
+    //        //for(int i = 0; i < n  ;  i++)
+    //        for(int i = 1; i <= n; ++i)
+    //        {
+    //            // output the number of the current row
+    //            std::cout << i;
+    //            for(int j = 1; j <= n; j += 1)
+    //            {
+    //                std::cout << '\t' << i * j;
+    //            }
+    //            std::cout << '\n';     // the same as using std::endl
+    //        }
+    //    }
+    //    else
+    //        std::cout << "The input value of n is wrong";
 
     return 0;
 }
