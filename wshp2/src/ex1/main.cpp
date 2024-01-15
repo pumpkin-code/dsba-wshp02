2f4166cf61b2e489d70760f4f09fe5a132bbb05c
KekaSick <mlggg@bk.ru>
2024-01-15 18:58:18 +0300

    1st commit bruh

--- wshp2/src/ex1/main.cpp
+++ wshp2/src/ex1/main.cpp
@@ -17,7 +17,22 @@
 // the entrypoint of the application
 int main()
 {
-    std::cout << "Hello world!";
+
+    int size_int = sizeof(int);
+    int size_char = sizeof(char);
+    int size_short = sizeof(short);
+    int size_long = sizeof(long);
+    int size_longlong = sizeof(long long);
+    int size_double = sizeof(double);
+    int size_bool = sizeof(bool);
+
+    std::cout << size_int << std::endl;
+    std::cout << size_char << std::endl;
+    std::cout << size_short << std::endl;
+    std::cout << size_long << std::endl;
+    std::cout << size_longlong << std::endl;
+    std::cout << size_double << std::endl;  
+    std::cout << size_bool << std::endl;  
 
     // TODO: place your code starting from this line
 
